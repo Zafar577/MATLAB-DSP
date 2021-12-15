5b6018e0f2fd7b7e46a8e77a21d9e64f6eafe905
Zafar Ahmad <72404761+Zafar577@users.noreply.github.com>
2021-12-15 07:39:21 +0500

    Add files via upload

---
+++ BandPass.m
@@ -0,0 +1,13 @@
+clear all,clc;
+Fs = 8000;           % Sampling frequency
+nBits=24;             % Data Rate
+nChannels=1;          % Microphone
+R = audiorecorder(Fs,nBits, nChannels) % used for activating Audio Reco..
+disp("Lets Generate Voice")
+recordblocking(R, 5);  % Stop Recording
+disp("Times Up");
+z = getaudiodata(R);    % Create an array from the audio signal
+fs = 8000;
+t = 0:1/fs:1;
+x = z            
+bandpass(x,[400 800],fs) 
